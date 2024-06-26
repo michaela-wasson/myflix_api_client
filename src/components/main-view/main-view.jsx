e4bb1a69cb268d39e52f8eb79c5ed17aba2b0c8c
michaela-wasson <108202135+michaela-wasson@users.noreply.github.com>
2024-06-25 17:41:48 -0700

    Updated with props

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,34 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "The Eloquent Iris",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Iris_sanguinea.JPG/330px-Iris_sanguinea.JPG",
-      description: "Art-House Film that consists of 90 minutes of watching an iris grow.",
-      genre: "Thriller"
-    },
-    {
-      id: 2,
-      title: "The Silence in the Attic",
-      image:
-        "https://studyfinds.org/wp-content/uploads/2023/10/shutterstock_1151252645-scaled.jpg",
-      description: "A small cat travels upstairs into the attic to hunt down and kill the scurrying mice.",
-      genre: "Musical"
-    },
-    {
-      id: 3,
-      title: "Learning How to Write Code",
-      image:
-        "https://cdn.create.vista.com/api/media/small/247763998/stock-photo-attractive-businesswoman-sitting-table-laptop-typing-office",
-      description: "A horror film that will keep you on the edge of your seat. Follow the trauma of the terrified student as she launches into api's and drowns in JavaScript.", 
-      genre: "Thriller"
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("https://movieapi2020-67bf919e3b74.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        
+        const moviesFromApi = data.map((movies) => {
+          return {
+            _id: movie._id,
+            Title: movie.title,
+            Description: movie.description,
+            Genre: {
+              name: movie.genre.name,
+              description: movie.genre.description
+            },
+            Director: {
+              name: movie.director.name,
+              bio: movie.director.bio,
+              birth: movie.director.birth,
+              death: movie.director.death
+            },
+            ImagePath: movie.image.url, 
+            Featured: movie.featured
+          }
+  });
+
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
